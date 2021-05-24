81350716d4ef3370d6d947a060bc7149a504094d
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-05-24 19:58:49 -0300

    saving user data

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,5 +1,13 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
 import { api } from "../services/api";
+import Router from 'next/router'
+
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+}
 
 type SignInCredentials = {
   email: string;
@@ -9,7 +17,7 @@ type SignInCredentials = {
 type AuthContextData = {
   signIn(credentials: SignInCredentials): Promise<void>;
   isAuthenticated: boolean;
-
+  user: User;
 }
 
 type AuthProviderProps = {
@@ -20,7 +28,10 @@ export const AuthContext = createContext({} as AuthContextData)
 
 export function AuthProvider({children}: AuthProviderProps) {
 
-  const isAuthenticated = false;
+  const [user, setUser] = useState<User>(null);
+
+
+  const isAuthenticated = !!user;
 
   async function signIn({email, password}: SignInCredentials) {
     try {
@@ -28,8 +39,17 @@ export function AuthProvider({children}: AuthProviderProps) {
         email,
         password,
       })
-  
-      console.log(response.data)
+      
+      const { token, refreshToken, permissions, roles } = response.data;
+
+      setUser({
+        email,
+        permissions,
+        roles
+      })
+
+      Router.push('/dashboard');
+
     }
 
     catch (err){
@@ -39,7 +59,7 @@ export function AuthProvider({children}: AuthProviderProps) {
   }
 
   return (
-    <AuthContext.Provider value={ {signIn, isAuthenticated} } >
+    <AuthContext.Provider value={ {signIn, isAuthenticated, user} } >
       {children}
     </AuthContext.Provider>
   )
