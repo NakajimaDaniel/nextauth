ff313d1a8d00093548fb6b8119a5aa7fa9efabc5
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-05-26 19:29:20 -0300

    refresh token

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,9 +1,15 @@
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { AuthContext } from "../contexts/AuthContext"
+import { api } from "../services/api"
 
 export default function Dashboard() {
 
   const {user} = useContext(AuthContext)
+
+  useEffect(()=> {
+    api.get('/me').then(response => console.log(response)).catch()
+  }, [])
+
 
   return (
     <h1>{user?.email}</h1>
