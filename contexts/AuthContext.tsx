d2168fd47cbb241f8f7938cf5c72a3ec4a8b2daa
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-05-28 19:24:23 -0300

    automatic logout

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,7 +1,7 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
 import { api } from "../services/api";
 import Router from 'next/router'
-import { parseCookies, setCookie } from 'nookies'
+import { destroyCookie, parseCookies, setCookie } from 'nookies'
 
 type User = {
   email: string;
@@ -26,6 +26,14 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData)
 
+export function signOut() {
+  destroyCookie(undefined, 'nextauth.token')
+  destroyCookie(undefined, 'nextauth.refreshToken')
+
+  Router.push('/')
+}
+
+
 export function AuthProvider({children}: AuthProviderProps) {
 
   const [user, setUser] = useState<User>(null);
@@ -39,6 +47,9 @@ export function AuthProvider({children}: AuthProviderProps) {
       api.get('/me').then(response=> {
         const { email, permissions, roles } = response.data;
         setUser({email, permissions, roles})
+      })
+      .catch(() => {
+        signOut();
       })
     }
 
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -7,7 +7,9 @@ export default function Dashboard() {
   const {user} = useContext(AuthContext)
 
   useEffect(()=> {
-    api.get('/me').then(response => console.log(response)).catch()
+    api.get('/me')
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
   }, [])
 
 
